5861922bca681c9c6e009434ff24a760db2be36d
miguelefs <87790409+miguelefs@users.noreply.github.com>
2021-08-02 11:19:56 -0300

    Criando Jogo_DoWhile.ino

---
+++ E7T3/Jogo_ComDoWhile.ino
@@ -0,0 +1,28 @@
+/*Tarefa de Computação 5: E7T3
+<Circuito do "Jogo, acerte o LED" com comando DO WHILE.> 
+Elaborada por: <Miguel Escorsin F.S>
+Data: <07/04/21>
+*/
+void setup()
+{
+  pinMode(11, INPUT);
+  int a=0;
+  do{
+    pinMode(a,OUTPUT);
+    a++;
+  } while(a<=10);
+}
+
+void loop()
+{
+int i=0;
+  do{
+    digitalWrite(i, HIGH);
+    delay(50);
+    while(digitalRead(11)==HIGH){
+      delay(1000);
+    }
+    digitalWrite(i, LOW);
+    i++;
+} while(i<=10);
+}
