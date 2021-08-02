27f0f4659d08d33f7ec357787f987fabf49ce2d7
miguelefs <87790409+miguelefs@users.noreply.github.com>
2021-08-02 11:09:56 -0300

    Criando Botão_LED.ino

---
+++ E4T1/Botão_LED.ino
@@ -0,0 +1,22 @@
+/*Tarefa de Computação 2: E4T1
+<circuito botão e LED.> 
+Elaborada por: <Miguel Escorsin F.S>
+Data: <12/03/21>
+*/
+
+void setup()
+{
+  pinMode(7, INPUT);
+  pinMode(6, INPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop()
+{
+  if (digitalRead(7)){
+    digitalWrite(10, HIGH);
+  }
+  if (digitalRead(6)){
+  digitalWrite(10, LOW);
+}
+}
