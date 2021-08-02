bb712c3fb439a797e33c8ee3fb2e6b0abb2e44cb
miguelefs <87790409+miguelefs@users.noreply.github.com>
2021-08-02 11:17:11 -0300

    Criando máx_mín.ino

---
+++ E6T3/Intensidade_máx_mín.ino
@@ -0,0 +1,22 @@
+/*Tarefa de Computação 4: E6T3
+<circuito intensidade variando entre máximo e mínimo.> 
+Elaborada por: <Miguel Escorsin F.S>
+Data: <29/03/21>
+*/
+int ledPin=9;
+int intensidade=0;
+int incrementar=1;
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+analogWrite(ledPin, intensidade);
+  delay(15);
+  intensidade=intensidade+incrementar;
+  if(intensidade==0||intensidade==255){
+    incrementar--;
+}
+}
