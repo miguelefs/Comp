856495ca9ae0134c0590ba27c0feac7efecbac36
miguelefs <87790409+miguelefs@users.noreply.github.com>
2021-08-02 11:19:12 -0300

    Criando Jogo_While.ino

---
+++ E7T2/Jogo_ComWhile.ino
@@ -0,0 +1,26 @@
+/*Tarefa de Computação 5: E7T2
+<Circuito do "Jogo, acerte o LED" com comando WHILE.> 
+Elaborada por: <Miguel Escorsin F.S>
+Data: <07/04/21>
+*/
+void setup()
+{
+  pinMode(11, INPUT);
+  for(int a=0;a<=10;a++){
+    pinMode(a,OUTPUT);
+  }
+}
+
+void loop()
+{
+int i=0;
+  while(i<=10){
+    digitalWrite(i, HIGH);
+    delay(50);
+    while(digitalRead(11)==HIGH){
+      delay(1000);
+    }
+    digitalWrite(i, LOW);
+    i++;
+}
+}
